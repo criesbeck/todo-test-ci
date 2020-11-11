fcdcd57d20e08e09641eee739c912c55b0ed83a2
Chris Riesbeck <chris.riesbeck@gmail.com>
2020-11-11 10:10:25 -0600

    add cypress add github actions

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// source:  https://dev.to/aveb/building-a-react-native-app-w-expo-77l
+import React, { useState } from 'react';
+import { 
+   StyleSheet,
+   Text,
+   View,
+   TextInput,
+   Button,
+   FlatList
+  } from 'react-native';
 
 export default function App() {
+
+  const [textInput, setTextInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const pressHandler = () => {
+    setTodos([textInput, ...todos]);
+  };
+
+  const typingHandler = (value) => {
+    setTextInput(value);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Quick and Dirty ToDo</Text>
+      <TextInput
+        onChangeText={typingHandler}
+        onSubmitEditing={pressHandler}
+        value={textInput}
+        style={{ borderWidth: 1, width: 300 }}
+      />
+      <Button
+        onPress={pressHandler}
+        title="Add Todo"
+      />
+      <FlatList
+        data={todos}
+        renderItem={todo => <Todo text={todo.item}/>}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const Todo = props => (
+  <View 
+    style={{ backgroundColor: "#eaeaea", width: 300, margin: 5 }}>
+    <Text>{props.text}</Text>
+  </View>
+);
